7ca40f396a475fceccd07088d426744c2ad83ef6
chibataku0815 <chiba@fores-tone.co.jp>
2022-09-09 17:45:40 +0900

    update localization

--- src/components/Survey/config/localization.js
+++ src/components/Survey/config/localization.js
@@ -2,4 +2,13 @@ import { localization } from "survey-creator-core";
 import "survey-creator-core/survey-creator-core.i18n.js";
 
 export const jaLocale = localization.getLocale("ja");
+/*
+
+schema:　https://github.com/surveyjs/survey-creator/blob/bc1a19a3789c057ba5bc924d5cdbcbd735edbdb4/packages/survey-creator-core/src/localization/japanese.ts
+
+examples
+jaLocale.ed.addNewQuestion = "Neue Frage";
+jaLocale.ed.addNewTypeQuestion = "Neue {0}";
+*/
+
 localization.currentLocale = "ja";
