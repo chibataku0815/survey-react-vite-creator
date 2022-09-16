489011aeb927ad324c0df429bdcf0bc74f451443
chiba-showcase-tv <chiba.takumi@showcase-tv.com>
2022-09-16 17:37:31 +0900

    custum sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ReactElementFactory } from 'survey-react-ui';
 import './App.css';
 import './components/Survey/OverridePropertyGrid';
+import CustomPropertyGridWrapper from './components/Survey/OverridePropertyGrid';
 import SurveyComponent from './components/Survey/SurveyCreatorComponent';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    ReactElementFactory.Instance.registerElement('svc-property-grid', (props) => {
+      return React.createElement(CustomPropertyGridWrapper, props);
+    });
+    return () => {};
+  }, []);
 
   return (
     <div className="App">
