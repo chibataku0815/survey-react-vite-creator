93a2f0c35fad931a2c9f351647af5c12e96acd9a
chibataku0815 <chiba@fores-tone.co.jp>
2022-12-13 15:52:48 +0900

    feat: add link toolbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { ReactElementFactory } from 'survey-react-ui';
 import './App.css';
 import './components/Survey/OverridePropertyGrid';
-import CustomPropertyGridWrapper from './components/Survey/OverridePropertyGrid';
 import SurveyComponent from './components/Survey/SurveyCreatorComponent';
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    ReactElementFactory.Instance.registerElement('svc-property-grid', (props) => {
-      return React.createElement(CustomPropertyGridWrapper, props);
-    });
     return () => {};
   }, []);
 
--- src/components/Survey/SurveyCreatorComponent.jsx
+++ src/components/Survey/SurveyCreatorComponent.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { ComponentCollection, Serializer } from 'survey-core';
 import 'survey-core/defaultV2.css';
 import 'survey-creator-core/survey-creator-core.css';
 import { SurveyCreator, SurveyCreatorComponent } from 'survey-creator-react';
@@ -9,6 +10,82 @@ import './config/localization';
 import { visibledProperties } from './config/propertiesGrid';
 import { setToolboxDefaltValue } from './config/toolbox';
 import { orderToolboxItem } from './config/toolboxItem';
+const replaceHtml = (url, text, isEnableTarget) => {
+  const newURL = url || '';
+  const newText = text || newURL;
+  const target = isEnableTarget ? "target='_blank'" : '';
+  return `<a href='${newURL}' rel='noopener noreferrer' ${target}>${newText}</a>`;
+};
+ComponentCollection.Instance.add({
+  name: 'link',
+  title: 'リンク',
+  iconName: 'icon-link',
+  questionJSON: {
+    type: 'html',
+    html: '',
+    showTitleLocation: false,
+  },
+  onInit() {
+    Serializer.addProperty('link', {
+      name: 'URL:text',
+      category: 'general',
+    });
+    Serializer.addProperty('link', {
+      name: '新規タブを開く:boolean',
+      category: 'general',
+      defaultValue: true,
+      default: true,
+    });
+
+    Serializer.addProperty('link', {
+      name: 'titleLocation',
+      visible: false,
+      default: 'hidden',
+    });
+    Serializer.addProperty('link', {
+      name: 'description',
+      visible: false,
+    });
+    Serializer.addProperty('link', {
+      name: 'hidenumber',
+      visible: false,
+    });
+    Serializer.addProperty('link', {
+      name: 'isRequired',
+      visible: false,
+    });
+    Serializer.addProperty('link', {
+      name: 'visible',
+      visible: false,
+      default: true,
+    });
+    Serializer.addProperty('link', {
+      name: 'validators',
+      visible: false,
+    });
+    Serializer.addProperty('link', {
+      name: 'requiredErrorText',
+      visible: false,
+    });
+  },
+
+  // itemをload時にaタグへ変換
+  onLoaded(question) {
+    question.contentQuestion.html = replaceHtml(question.URL || '', question.title, question['新規タブを開く']);
+  },
+
+  // プロパティに変更があった際にaタグへ変換
+  onPropertyChanged(question) {
+    question.contentQuestion.html = replaceHtml(question.URL || '', question.title, question['新規タブを開く']);
+  },
+
+  // メソッドを記載しないとエラーになるため記載
+  onAfterRender() {},
+  onAfterRenderContentElement() {},
+  onCreated() {},
+  onItemValuePropertyChanged() {},
+  onValueChanged() {},
+});
 
 function SurveyComponent() {
   const creator = new SurveyCreator(creatorOptions);
@@ -22,11 +99,7 @@ function SurveyComponent() {
     return () => {};
   }, []);
 
-  return (
-    <div>
-      <SurveyCreatorComponent creator={creator} renderElement={(data) => <h1>Hello {data.target}</h1>} />
-    </div>
-  );
+  return <SurveyCreatorComponent creator={creator} />;
 }
 
 export default SurveyComponent;
