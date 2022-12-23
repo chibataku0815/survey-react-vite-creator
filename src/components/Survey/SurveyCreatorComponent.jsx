901ad646b751a4fd448200182992187571b4c61a
chibataku0815 <chiba@fores-tone.co.jp>
2022-12-23 17:59:54 +0900

    feat: fix bugs

--- src/components/Survey/SurveyCreatorComponent.jsx
+++ src/components/Survey/SurveyCreatorComponent.jsx
@@ -13,91 +13,6 @@ import { visibledProperties } from './config/propertiesGrid';
 import { setToolboxDefaltValue } from './config/toolbox';
 import { orderToolboxItem } from './config/toolboxItem';
 
-// Register our widget in singleton custom widget collection
-const widget = {
-  name: 'submitbuttonColor',
-  isFit: (question) => {
-    return question.getType() === 'text';
-  },
-  isDefaultRender: true,
-  afterRender: (question, el) => {
-    // aria-label='color'を持つ要素を取得
-    const input = el.querySelector("[aria-label='Submitbutton color']");
-
-    if (!input) return;
-
-    input.style.width = '80%';
-    input.style.marginRight = '10px';
-    if (!input) return;
-    input.type = question.inputType;
-    el.style.display = 'flex';
-    el.style.alignItems = 'center';
-
-    const pickrDiv = document.createElement('div');
-
-    // .pickrを持つ要素を取得
-    const hasPickr = el.querySelector('.pickr');
-    // inputの後ろにpickrDivを追加
-    console.info(hasPickr === null);
-    if (!hasPickr) {
-      input.after(pickrDiv);
-    } else {
-      return;
-    }
-
-    const pickrInstance = new Pickr({
-      el: pickrDiv,
-      default: '#303030',
-      theme: 'monolith',
-      lockOpacity: true,
-      comparison: false,
-
-      swatches: ['#3f51b5', '#f44336', '#e91e63', '#9c27b0', '#673ab7'],
-
-      components: {
-        preview: true,
-        opacity: false,
-        hue: true,
-
-        interaction: {
-          rgba: false,
-          input: true,
-          save: true,
-        },
-      },
-      i18n: {
-        // Strings visible in the UI
-        'ui:dialog': 'color picker dialog',
-        'btn:toggle': 'toggle color picker dialog',
-        'btn:swatch': 'color swatch',
-        'btn:last-color': 'use previous color',
-        'btn:save': '色一覧に追加',
-        'btn:cancel': 'Cancel',
-        'btn:clear': 'Clear',
-
-        // Strings used for aria-labels
-        'aria:btn:save': 'save and close',
-        'aria:btn:cancel': 'cancel and close',
-        'aria:btn:clear': 'clear and close',
-        'aria:input': 'color input field',
-        'aria:palette': 'color selection area',
-        'aria:hue': 'hue selection slider',
-        'aria:opacity': 'selection slider',
-      },
-    })
-      .on('init', () => {
-        pickrInstance.setColor(input.value);
-        const pcrResult = document.querySelector('.pcr-result');
-        if (pcrResult) pcrResult.maxLength = 7;
-      })
-      .on('change', (color) => {
-        input.value = color.toHEXA().toString(0);
-      });
-    input.value = el.value;
-  },
-};
-CustomWidgetCollection.Instance.addCustomWidget(widget);
-
 function SurveyComponent() {
   const creator = new SurveyCreator(creatorOptions);
   visibledProperties(creator);
@@ -105,6 +20,92 @@ function SurveyComponent() {
   orderToolboxItem(creator);
   setToolboxDefaltValue(creator);
   visibledProperties(creator);
+
+  // Register our widget in singleton custom widget collection
+  const widget = {
+    name: 'submitbuttonColor',
+    isFit: (question) => {
+      return question.getType() === 'text';
+    },
+    isDefaultRender: true,
+    afterRender: (question, el) => {
+      // aria-label='color'を持つ要素を取得
+      const input = el.querySelector("[aria-label='Submitbutton color']");
+
+      if (!input) return;
+
+      input.style.width = '80%';
+      input.style.marginRight = '10px';
+      if (!input) return;
+      input.type = question.inputType;
+      el.style.display = 'flex';
+      el.style.alignItems = 'center';
+
+      const pickrDiv = document.createElement('div');
+
+      // .pickrを持つ要素を取得
+      const hasPickr = el.querySelector('.pickr');
+      // inputの後ろにpickrDivを追加
+      console.info(hasPickr === null);
+      if (!hasPickr) {
+        input.after(pickrDiv);
+      } else {
+        return;
+      }
+
+      const pickrInstance = new Pickr({
+        el: pickrDiv,
+        default: '#303030',
+        theme: 'monolith',
+        lockOpacity: true,
+        comparison: false,
+
+        swatches: ['#3f51b5', '#f44336', '#e91e63', '#9c27b0', '#673ab7'],
+
+        components: {
+          preview: true,
+          opacity: false,
+          hue: true,
+
+          interaction: {
+            rgba: false,
+            input: true,
+            save: true,
+          },
+        },
+        i18n: {
+          // Strings visible in the UI
+          'ui:dialog': 'color picker dialog',
+          'btn:toggle': 'toggle color picker dialog',
+          'btn:swatch': 'color swatch',
+          'btn:last-color': 'use previous color',
+          'btn:save': '色一覧に追加',
+          'btn:cancel': 'Cancel',
+          'btn:clear': 'Clear',
+
+          // Strings used for aria-labels
+          'aria:btn:save': 'save and close',
+          'aria:btn:cancel': 'cancel and close',
+          'aria:btn:clear': 'clear and close',
+          'aria:input': 'color input field',
+          'aria:palette': 'color selection area',
+          'aria:hue': 'hue selection slider',
+          'aria:opacity': 'selection slider',
+        },
+      })
+        .on('init', () => {
+          pickrInstance.setColor(input.value);
+          const pcrResult = document.querySelector('.pcr-result');
+          if (pcrResult) pcrResult.maxLength = 7;
+        })
+        .on('change', (color) => {
+          input.value = color.toHEXA().toString(0);
+          // inputへフォーカス
+          input.focus();
+        });
+    },
+  };
+  CustomWidgetCollection.Instance.addCustomWidget(widget);
 
   // Serializer.addProperties custom widget
   JsonObject.metaData.addProperty('survey', {
