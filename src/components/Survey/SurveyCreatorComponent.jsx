256a7e172ba2a7b7e439eeac240354a77a162640
chiba-showcase-tv <chiba.takumi@showcase-tv.com>
2023-01-17 15:04:11 +0900

    feat: custom preview panel

--- src/components/Survey/SurveyCreatorComponent.jsx
+++ src/components/Survey/SurveyCreatorComponent.jsx
@@ -1,3 +1,4 @@
+import { css } from '@linaria/core';
 import Pickr from '@simonwep/pickr';
 import '@simonwep/pickr/dist/themes/monolith.min.css';
 import { useEffect } from 'react';
@@ -100,11 +101,20 @@ function SurveyComponent() {
         })
         .on('change', (color) => {
           input.value = color.toHEXA().toString(0);
+          document.body.style.setProperty('--global-color-custom', input.value);
+          css`
+            :global() {
+              .sd-btn--action {
+                background-color: var(--global-color-custom);
+              }
+            }
+          `;
           // inputへフォーカス
           input.focus();
         });
     },
   };
+
   CustomWidgetCollection.Instance.addCustomWidget(widget);
 
   // Serializer.addProperties custom widget
@@ -120,6 +130,13 @@ function SurveyComponent() {
     if (options.propertyName === 'completedHtml') {
       options.propertyHtml = '';
     }
+  });
+
+  //
+  Serializer.addProperty('survey', {
+    name: 'completedText:string',
+    category: 'general',
+    default: '送信',
   });
 
   Serializer.addProperty('survey', {
@@ -159,6 +176,40 @@ function SurveyComponent() {
         options.error = '正しいカラーコードを入力してください（例：#000000）';
       }
     }
+  });
+
+  creator.onTestSurveyCreated.add(async (sender, options) => {
+    creator.JSON.completedText
+      ? (options.survey.completeText = creator.JSON.completedText)
+      : (options.survey.completeText = '送信');
+
+    // show svg
+    options.survey.completedHtml = `
+      <div class="survey-completed">
+        <div class="survey-completed__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+            <g fill="none" fill-rule="evenodd">
+              <circle cx="50" cy="50" r="50" fill="#FFF" />
+              <path fill="#3F51B5" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+              <path fill="#FFF" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+              <path fill="#3F51B5" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+              <path fill="#FFF" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+              <path fill="#3F51B5" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+              <path fill="#FFF" d="M50 100C22.4 100 0 77.6 0 50S22.4 0 50 0s50 22.4 50 50-22.4 50-50 50zm0-93C24.5 7 7 24.5 7 50s17.5 43 43 43 43-17.5 43-43S75.5 7 50 7z" />
+
+            </g>
+          </svg>
+        </div>
+        <div class="survey-completed__title">送信完了</div>
+        <div class="survey-completed__text">ご協力ありがとうございました。</div>
+      </div>
+    `;
+
+    options.survey.onComplete.add(async (sender, options) => {
+      console.log('onComplete');
+      const survey = options;
+      console.log(survey);
+    });
   });
 
   useEffect(() => {
